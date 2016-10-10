1befc6a2b887edaedf52832e9f45b13e2f9b452b
kakahikari <kakahikari@gmail.com>
2016-10-11 07:53:24 +0800

    新增 桌面初版

--- js/script.js
+++ js/script.js
@@ -1,41 +1,25 @@
 $(function(){
-  $(document).on('scroll',function() {
-    var _scrollTop = $(this).scrollTop();
-    if(_scrollTop > 340) {
-      $('nav').addClass('js-active');
-      $('nav li').removeClass('js-active');
-      if(_scrollTop < $('#page2').offset().top - 94)
-        $('nav li').eq(0).addClass('js-active');
-      else
-      if(_scrollTop < $('#page3').offset().top - 94)
-        $('nav li').eq(1).addClass('js-active');
-      else
-      if(_scrollTop < $('#page4').offset().top - 94)
-        $('nav li').eq(2).addClass('js-active');
-      else
-      if(_scrollTop < $('#page5').offset().top - 94)
-        $('nav li').eq(3).addClass('js-active');
-      else
-      if(_scrollTop < $('#page6').offset().top - 94)
-        $('nav li').eq(4).addClass('js-active');
-      else
-      if(_scrollTop < $('#page7').offset().top - 94)
-        $('nav li').eq(5).addClass('js-active');
-    }
-    else
-      $('nav').removeClass('js-active');
+  $(document).on('click','nav.index2 li a',function() {
+    var _index = $("nav.index2 li").index($(this).parent())-1;
+    $('nav.index2 li a').removeClass('js-active');
+    $(this).addClass('js-active');
+    $('.container.index2 article').removeClass('js-active');
+    $('.container.index2 article').eq(_index).addClass('js-active')
   });
-  $(document).on('click','.tabs li',function() {
-    var _index = $(".tabs li").index(this);
-     $('.tabs li').removeClass('js-active');
-     $(this).addClass('js-active');
-     $('.tabs-content .item').removeClass('js-active');
-     $('.tabs-content .item').eq(_index).addClass('js-active');
-  });
-  $(document).on('click','.more',function() {
-    $(this).toggleClass('js-active');
-  });
-  $(document).on('click','.help',function() {
-    $(this).toggleClass('js-active');
-  });
+  var menu = GetURLParameter('menu');
+  $('nav.index2 li').eq(menu).children('a').click();
 });
+
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}​
